388bc8cf6f3bd9791f0d5f1f83c6164775f5ded8
Paul Norling <paulnorling87@gmail.com>
2022-12-29 14:55:28 -0600

    working on favorites get route

--- src/components/FavoritesPage/FavoritesPage.jsx
+++ src/components/FavoritesPage/FavoritesPage.jsx
@@ -1,6 +1,15 @@
+import React, { useEffect } from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import FavoritesPageCard from "./FavoritesPageCard";
 
 function FavoritesPage() {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch({ type: 'FETCH_FAVORITES_BY_USER' });
+    }, []);
+
     return (
         <FavoritesPageCard/>
     )
