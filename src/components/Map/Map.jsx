26890ed69eb0755cf7d0b582964e5e5776a880df
Paul Norling <paulnorling87@gmail.com>
2023-01-05 19:35:56 -0600

    places array displaying on map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,7 +5,7 @@ import './Map.css'
 
 function Map(){
 
-    const places = [{lat: 44.9778, lng: -93.2650}, {lat: 45.16173214652718, lng: -92.71747537315042}, {lat: 44.9537, lng: -93.0900}]
+    const places = [{lat: 44.9778, lng: -93.2650}, {lat: 45.16173214652718, lng: -92.71747537315042}, {lat: 44.9537, lng: -93.0900}, {lat: 44, lng: -93}]
 
     const history = useHistory();
 
@@ -29,16 +29,16 @@ function Map(){
               center={{lat: 44.9778, lng: -93.2650}}
               mapContainerClassName="map-container"
             >
-            {/* {places.map(location => {
+            {places.map(location => {
               return (
                 <div key ={location.lat}>
-                  <MarkerF onLoad={onLoad} position= {{location}}/>
+                  <MarkerF onLoad={onLoad} position={location}/>
                 </div>
               )
-            })} */}
-              <MarkerF onClick={mpls} onLoad={onLoad} position= {{lat: 44.9778, lng: -93.2650}}/>
+            })}
+              {/* <MarkerF onClick={mpls} onLoad={onLoad} position= {{lat: 44.9778, lng: -93.2650}}/>
               <MarkerF onLoad={onLoad} position= {{lat: 45.16173214652718, lng: -92.71747537315042}}/>
-              <MarkerF onLoad={onLoad} position= {{lat: 44.9537, lng: -93.0900}}/>
+              <MarkerF onLoad={onLoad} position= {{lat: 44.9537, lng: -93.0900}}/> */}
             </GoogleMap>
             </LoadScript>
     )
