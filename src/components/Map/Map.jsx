f9fb762204b5b1ace0cade89712dc0052153b871
Paul Norling <paulnorling87@gmail.com>
2023-01-06 13:50:18 -0600

    hide key

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,7 +5,7 @@ import './Map.css'
 
 function Map(){
 
-    const places = [{lat: 44.9778, lng: -93.2650}, {lat: 45.16173214652718, lng: -92.71747537315042}, {lat: 44.9537, lng: -93.0900}, {lat: 44, lng: -93}]
+    const places = [ {lat: 45.16173214652718, lng: -92.71747537315042}, {lat: 44.9537, lng: -93.0900}, {lat: 44.95, lng: -93}]
 
     const history = useHistory();
 
@@ -18,29 +18,26 @@ function Map(){
         history.push('/about')
     }
 
-
+    
     return (
-        
-        <LoadScript
-            //   googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-            googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-            <GoogleMap 
-              zoom={10} 
-              center={{lat: 44.9778, lng: -93.2650}}
-              mapContainerClassName="map-container"
-            >
-            {places.map(location => {
-              return (
-                <div key ={location.lat}>
-                  <MarkerF onLoad={onLoad} position={location}/>
-                </div>
-              )
-            })}
-              {/* <MarkerF onClick={mpls} onLoad={onLoad} position= {{lat: 44.9778, lng: -93.2650}}/>
-              <MarkerF onLoad={onLoad} position= {{lat: 45.16173214652718, lng: -92.71747537315042}}/>
-              <MarkerF onLoad={onLoad} position= {{lat: 44.9537, lng: -93.0900}}/> */}
-            </GoogleMap>
-            </LoadScript>
+      <LoadScript
+      googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+      <GoogleMap 
+        zoom={10} 
+        center={{lat: 44.9778, lng: -93.2650}}
+        mapContainerClassName="map-container"
+      >
+      {places.map(location => {
+        console.log(location.lat)
+        return (
+          <div key ={location.lat}>
+            <MarkerF onLoad={onLoad} position={location}/>
+          </div>
+        )
+      })}
+        <MarkerF onClick={mpls} onLoad={onLoad} position= {{lat: 44.9778, lng: -93.2650}}/>
+      </GoogleMap>
+      </LoadScript>
     )
 
 }
