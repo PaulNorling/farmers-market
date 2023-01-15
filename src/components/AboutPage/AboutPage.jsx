27df5e4e01857ed69a5ad7d77c889148224b0156
Paul Norling <paulnorling87@gmail.com>
2023-01-15 08:28:14 -0600

    about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -5,14 +5,30 @@ function AboutPage() {
   return (
     <div className="about-container">
       <div>
-        <p>"Looking to shop small and support your community?
-          Our farmers market app connects consumers with local farmers and producers,
+        <p>Looking to shop small and support your community?
+          Farmer's Marketplace connects consumers with their neighbors,
           making it easy to find and purchase fresh, sustainably grown produce and goods.
-          With our app, farmers can list their fresh, locally-grown produce and consumers
-          can browse and purchase from the comfort of their own home.
+          
           Our app simplifies the process of supporting small businesses and eating healthy.
-          With our farmers market app, supporting local agriculture and eating healthy has never been easier."
+          With Farmer's Marketplace supporting local agriculture and eating healthy has never been easier.
         </p>
+      </div>
+      <div></div>
+      <div>
+          <h3>Technologies Used:</h3>
+          <div>React</div>
+          <div>Node.js</div>
+          <div>sweetalert2</div>
+          <div>react-google-maps/api</div>
+          <div>react-animated-slider</div>
+      </div>
+      <div>
+        <h3>Biggest Challenge</h3>
+        <p>Google maps</p>
+      </div>
+      <div>
+        <h3>Next Setps</h3>
+        <p>payment options</p>
       </div>
     </div>
   );
